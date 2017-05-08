a348a45e515d4c4725d1b334c1333de237d92357
Alex Mikhalev <alexmikhalevalex@gmail.com>
2017-05-08 09:34:12 -0600

    Fixed webpack output folders

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -11,7 +11,7 @@ module.exports = {
     ],
     devtool: "inline-source-map",
     output: {
-        path: path.resolve(__dirname, "build"),
+        path: path.resolve(__dirname, "../build"),
         filename: "bundle.js"
     },
     resolve: {
--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -8,7 +8,7 @@ module.exports = {
     ],
     devtool: "none",
     output: {
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "../dist"),
         filename: "bundle.js"
     },
     resolve: {
