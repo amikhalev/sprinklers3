fce64f38506f4357946a72dbbd2702df5de36700
Alex Mikhalev <alexmikhalevalex@gmail.com>
2018-09-02 23:29:29 -0600

    refactor: Move server entrypoints to bin folder

---
+++ server/bin/server.ts
@@ -0,0 +1,27 @@
+import * as http from "http";
+import * as WebSocket from "ws";
+
+import log from "@common/logger";
+import { createApp, ServerState, WebSocketApi } from "../";
+
+const state = new ServerState();
+const app = createApp(state);
+const webSocketApi = new WebSocketApi(state);
+
+const port = +(process.env.PORT || 8080);
+const host = process.env.HOST || "0.0.0.0";
+
+const server = new http.Server(app);
+const webSocketServer = new WebSocket.Server({ server });
+webSocketApi.listen(webSocketServer);
+
+state
+  .start()
+  .then(() => {
+    server.listen(port, host, () => {
+      log.info(`listening at ${host}:${port}`);
+    });
+  })
+  .catch(err => {
+    log.error({ err }, "error starting server");
+  });
--- server/index.ts
+++ server/index.ts
@@ -4,32 +4,6 @@ import "./configureAlias";
 import "./env";
 import "./configureLogger";
 
-import log from "@common/logger";
-import { Server } from "http";
-import * as WebSocket from "ws";
-
-import { ServerState } from "./state";
-import { createApp } from "./express";
-import { WebSocketApi } from "./sprinklersRpc/WebSocketApi";
-
-const state = new ServerState();
-const app = createApp(state);
-const webSocketApi = new WebSocketApi(state);
-
-const port = +(process.env.PORT || 8080);
-const host = process.env.HOST || "0.0.0.0";
-
-const server = new Server(app);
-const webSocketServer = new WebSocket.Server({ server });
-webSocketApi.listen(webSocketServer);
-
-state
-  .start()
-  .then(() => {
-    server.listen(port, host, () => {
-      log.info(`listening at ${host}:${port}`);
-    });
-  })
-  .catch(err => {
-    log.error({ err }, "error starting server");
-  });
+export { ServerState } from "./state";
+export { createApp } from "./express";
+export { WebSocketApi } from "./sprinklersRpc/";
